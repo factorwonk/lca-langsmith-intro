61699309b054989707a1d525092f52c14a83bc50
Abhishek Das <>
2026-02-17 07:54:42 +0530

    Had to modify notebook and utils to run basic langsmith trace

--- notebooks/module_0/utils.py
+++ notebooks/module_0/utils.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders.sitemap import SitemapLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import SKLearnVectorStore
 from langchain_openai import OpenAIEmbeddings
 
@@ -19,8 +19,23 @@ def get_vector_db_retriever():
         return vectorstore.as_retriever(lambda_mult=0)
 
     # Otherwise, index LangSmith documents and create new vector store
-    ls_docs_sitemap_loader = SitemapLoader(web_path="https://docs.smith.langchain.com/sitemap.xml", continue_on_failure=True)
-    ls_docs = ls_docs_sitemap_loader.load()
+    # ls_docs_sitemap_loader = SitemapLoader(web_path="https://docs.smith.langchain.com/sitemap.xml", continue_on_failure=False)
+    # ls_docs = ls_docs_sitemap_loader.load()
+    # print(ls_docs) # print the first 500 characters of the first document to verify that documents are being loaded correctly
+
+    # Load LangSmith documentation
+    url = [
+        "https://docs.langchain.com/langsmith/home"
+    ]
+    
+    ls_docs = []
+    try:
+        loader = WebBaseLoader(url)
+        docs = loader.load()
+        ls_docs.extend(docs)
+        print(f"Loaded {len(docs)} documents from {url}")
+    except Exception as e:
+        print(f"Error loading {url}: {e}")
 
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=500, chunk_overlap=0
